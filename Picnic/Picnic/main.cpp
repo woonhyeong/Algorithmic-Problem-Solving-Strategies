5660e153b3f3df1c3fc2630aa58741804531baab
owner <owner@OWNER-PC>
2018-02-09 17:09:20 +0900

    완성

--- Picnic/Picnic/main.cpp
+++ Picnic/Picnic/main.cpp
@@ -6,24 +6,49 @@
 
 #define MAX_CASE 50
 
+int st_num;
+bool areFriend[10][10];
+
 using namespace std;
 
 int countPair(bool taken[10]) {
 
+	int count = 0;
+	int firstFree = -1;
+
+	for (int i = 0; i < st_num; i++) {
+		if (!taken[i]) {
+			firstFree = i;
+			break;
+		}
+	}
+
+	if (firstFree == -1) return 1;
+
+	for (int k = firstFree+ 1; k < st_num; k++ ) {
+		if (!taken[k]&&areFriend[firstFree][k]) {
+			taken[firstFree] = taken[k] = true;
+			count += countPair(taken);
+			taken[firstFree] = taken[k] = false;
+		}
+	}
+
+	return count;
 }
 
 int main() {
 	int total_case;
-	bool areFriend[10][10];
+	vector<int> count;
 
 	cin >> total_case;
 	if (total_case > MAX_CASE) exit(-1);
 
 	for (int i = 0; i < total_case; i++) {
-		int st_num, pair_num;
+		int pair_num;
 		cin >> st_num >> pair_num;
 
 		bool taken[10] = { false, };
+		memset(areFriend, false, sizeof(areFriend));
 
 		if (st_num > 10 || st_num < 2) exit(-1);
 		if (pair_num < 0 || pair_num >(st_num*(st_num - 1) / 2)) exit(-1);
@@ -37,8 +62,12 @@ int main() {
 			areFriend[student1][student2] = true;
 			areFriend[student2][student1] = true;
 		}
+		count.push_back(countPair(taken));
 	}
 
+	for (vector<int>::iterator it = count.begin(); it != count.end(); it++) {
+		cout << *it << endl;
+	}
 
 	return 0;
 }
