9b140f332b5c1f52fa8ae159986f26df0a52cd9d
owner <owner@OWNER-PC>
2018-07-18 22:55:02 +0900

    [Boggle문제] 메모이제이션으로 해결/http://tadomstudio.tistory.com/17에서 힌트를 얻음/속도 0ms

--- Boggle/Boggle/boggle.cpp
+++ Boggle/Boggle/boggle.cpp
@@ -1,32 +1,36 @@
-#include <string>
+#include <string.h>
 #include <stdio.h>
-
+#include <cstdlib>
 #define FOR(i,n) for(int i = 0; i<(n);++i)
+
 const int dx[8] = { -1,0,1,-1,1,-1,0,1 };
 const int dy[8] = { -1,-1,-1,0,0,1,1,1 };
 char board[5][6];
+char table[5][5][10];
+char word[11];
+int wordLength;
 using namespace std;
 
 bool inRange(int x, int y) {
 	if (x < 0 || y < 0 || x>4 || y>4)
 		return false;
-
 	return true;
 }
 
-bool hasWord(int x, int y, const string& word) {
+bool hasWord(int x, int y, int index) {
 	if (!inRange(x, y))
 		return false;
-	if (board[x][y] != word[0])
-		return false;
-	if (word.size() == 1)
+	if (index + 1 >= wordLength)
 		return true;
 
-	for (int direction = 0; direction < 8; ++direction) {
+	FOR(direction, 8) {
 		int nextX = x + dx[direction], nextY = y + dy[direction];
-		if (hasWord(nextX, nextY, word.substr(1)))
+
+		if (inRange(nextX, nextY) && (table[nextX][nextY][index + 1] == 1) && (board[nextX][nextY] == word[index + 1]) && hasWord(nextX, nextY, index + 1))
 			return true;
 	}
+
+	table[x][y][index] = 0;
 
 	return false;
 }
@@ -38,25 +42,29 @@ int main(void) {
 	if (test < 0 || test>50)
 		exit(1);
 
-	while(test--) {
+	while (test--) {
 		int wordNum = 0;
-		char word[11];
 
 		FOR(i, 5) {
-			scanf_s("%s",board[i],sizeof(board[i]));
+			scanf_s("%s", board[i], sizeof(board[i]));
 		}
 
 		scanf_s("%d", &wordNum);
 		if (wordNum < 1 || wordNum >10)
 			exit(1);
 
-		while(wordNum--) {
+		while (wordNum--) {
 			bool check = false;
-			scanf_s("%s", word,sizeof(word));
+			scanf_s("%s", word, sizeof(word));
+			wordLength = strlen(word);
+			memset(table, 1, sizeof(table));
+
 			FOR(i, 5) {
 				FOR(j, 5) {
-					if (check = hasWord(i, j, word))
+					if ((board[i][j] == word[0]) && hasWord(i, j, 0)) {
+						check = true;
 						break;
+					}
 				}
 				if (check)
 					break;
